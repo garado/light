a7105fd29957278a38c7824907e59d0b066f631b
garado <alexisgarado@gmail.com>
2026-05-06 20:53:48 -0500

    chore: restructure build pythonpkgs stuff

--- flake.nix
+++ flake.nix
@@ -1,40 +1,82 @@
 {
-  description = "Unofficial Light Phone music API";
+  description = "Unofficial Light Phone API and CLI";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    pythonEnv = pkgs.python312.withPackages (p: with p; [
-      playwright
-      click
-      rich
-      rich-click
-      mutagen
-      keyring
-      secretstorage
-      textual
-      httpx
-      attrs
-    ]);
-    light = pkgs.writeShellScriptBin "light" ''
-      PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers} \
-      PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true \
-      PYTHONPATH=${./light_client}:${./light_api}:$PYTHONPATH \
-      ${pythonEnv}/bin/python -m light_cli_tui.cli "$@"
-    '';
+    python = pkgs.python312;
+
+    light-client = python.pkgs.buildPythonPackage {
+      pname = "light-client";
+      version = "1.0.0";
+      pyproject = true;
+      src = ./light_client;
+      build-system = [ python.pkgs.hatchling ];
+      dependencies = with python.pkgs; [ httpx attrs python-dateutil ];
+    };
+
+    light-api = python.pkgs.buildPythonPackage {
+      pname = "light-api";
+      version = "0.1.0";
+      pyproject = true;
+      src = ./light_api;
+      build-system = [ python.pkgs.hatchling ];
+      dependencies = with python.pkgs; [
+        keyring
+        mutagen
+        playwright
+        light-client
+      ];
+    };
+
+    light-cli-tui = python.pkgs.buildPythonPackage {
+      pname = "light-cli-tui";
+      version = "0.1.0";
+      pyproject = true;
+      src = ./light_cli_tui;
+      build-system = [ python.pkgs.hatchling ];
+      dependencies = with python.pkgs; [
+        click
+        rich-click
+        rich
+        textual
+        light-api
+      ];
+
+      postInstall = ''
+        wrapProgram $out/bin/light \
+          --set PLAYWRIGHT_BROWSERS_PATH ${pkgs.playwright-driver.browsers} \
+          --set PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS true
+      '';
+    };
+
   in {
-    packages.${system}.default = light;
+    packages.${system} = {
+      inherit light-api light-cli-tui;
+      default = light-cli-tui;
+    };
 
     apps.${system}.default = {
       type = "app";
-      program = "${light}/bin/light";
+      program = "${light-cli-tui}/bin/light";
     };
 
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
-        pythonEnv
+        (python.withPackages (p: with p; [
+          playwright
+          click
+          rich
+          rich-click
+          mutagen
+          keyring
+          secretstorage
+          textual
+          httpx
+          attrs
+        ]))
         uv
         playwright-driver.browsers
         pyright
@@ -44,6 +86,8 @@
       shellHook = ''
         export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
         export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+        export PYTHONPATH=${pkgs.python312}/lib/python3.12/site-packages:$PYTHONPATH
       '';
     };
   };
