cdd26fd1c3aa7076fd7af6628d03061e70b72e1f
garado <alexisgarado@gmail.com>
2026-07-21 10:55:15 -0500

    fix: tentative fix for cli credential flags not being accepted

--- light_api/light_api/__init__.py
+++ light_api/light_api/__init__.py
@@ -1,11 +1,20 @@
 import functools
+import click
 from .client import Light
 
 
 def with_light(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        with Light() as light:
+        obj = click.get_current_context().find_root().obj or {}
+        with Light(
+            email=obj.get("email"),
+            email_file=obj.get("email_file"),
+            password=obj.get("password"),
+            password_file=obj.get("password_file"),
+            phone=obj.get("phone_number"),
+            phone_file=obj.get("phone_number_file"),
+        ) as light:
             return f(light, *args, **kwargs)
 
     return wrapper
